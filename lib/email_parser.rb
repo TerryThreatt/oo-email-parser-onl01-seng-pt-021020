211665f284e3a5fa80d6632742741b8732ac5e5d
Terry Threatt <terry.threatt@gmail.com>
2020-03-04 21:40:26 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,6 +7,10 @@ class EmailAddressParser
   
   attr_accessor :email_list
   
+  def initialize(email_list)
+    @email_list = email_list
+  end 
+  
   def parse(email_list)
   email_addresses.split(",").join("")
   end 
